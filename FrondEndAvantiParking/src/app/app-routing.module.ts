fb9a7b8728d903510f8900061900cda208771277
AnthonyDaniel <39199700+AnthonyDaniel@users.noreply.github.com>
2019-04-20 20:20:10 -0600

    Create componets
    Navbar
    ErrorPage404
    Notifications
    Navbar
    Footer
    Home
    Hero
    Profile
    Register
    Login
    Reserve
    Vehicle

--- FrondEndAvantiParking/src/app/app-routing.module.ts
+++ FrondEndAvantiParking/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Errorpage404Component } from './components/errorpage404/errorpage404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //All router here 
+
+  { path: '**' , component: Errorpage404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrondEndAvantiParking/src/app/app.module.ts
+++ FrondEndAvantiParking/src/app/app.module.ts
@@ -3,14 +3,55 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { SlideshowModule} from 'ng-simple-slideshow';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Errorpage404Component } from './components/errorpage404/errorpage404.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { ReserveComponent } from './components/reserve/reserve.component';
+import { VehicleComponent } from './components/vehicle/vehicle.component';
+import { NotificationsComponent } from './components/notifications/notifications.component'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    Errorpage404Component,
+    HeroComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    ReserveComponent,
+    VehicleComponent,
+    NotificationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    CommonModule,
+    NgbModalModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    SlideshowModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
