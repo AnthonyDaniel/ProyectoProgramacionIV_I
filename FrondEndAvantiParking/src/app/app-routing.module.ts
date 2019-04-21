54354af018630aa554953580c0c807cb3b846b29
AnthonyDaniel <39199700+AnthonyDaniel@users.noreply.github.com>
2019-04-20 21:52:10 -0600

    Update app-routing.module.ts

--- FrondEndAvantiParking/src/app/app-routing.module.ts
+++ FrondEndAvantiParking/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Errorpage404Component } from './components/errorpage404/errorpage404.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
 
 const routes: Routes = [
   //All router here 
-
+  { path: 'user/register' , component: RegisterComponent},
+  { path: 'user/login' , component: LoginComponent },
   { path: '**' , component: Errorpage404Component }
 ];
 
