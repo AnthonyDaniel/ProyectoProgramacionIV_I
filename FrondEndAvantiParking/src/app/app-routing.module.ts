fcd112065d724e04d7195500fe9e3949ddf4f8b2
KelinSilva <kasvillarrealgmail.com>
2019-04-24 22:22:46 -0600

    adminModule

--- FrondEndAvantiParking/src/app/app-routing.module.ts
+++ FrondEndAvantiParking/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { Errorpage404Component } from './components/errorpage404/errorpage404.co
 import { LoginComponent } from './components/user/login/login.component';
 import { RegisterComponent } from './components/user/register/register.component';
 import { ReserveComponent } from './components/reserve/reserve.component';
-
+import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdminHomeComponent } from './components/administration/admin-home/admin-home.component';
+ 
 const routes: Routes = [
   //All router here 
+  { path: '' , component:HomeComponent },
   { path: 'reserve' , component: ReserveComponent },
   { path: 'user/register' , component: RegisterComponent},
   { path: 'user/login' , component: LoginComponent },
+  { path: 'hero' , component: HeroComponent },
+  { path: 'administration/admin-home' , component: AdminHomeComponent},
   { path: '**' , component: Errorpage404Component }
 ];
 
