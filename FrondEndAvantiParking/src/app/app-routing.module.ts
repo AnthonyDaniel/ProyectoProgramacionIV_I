5323f8810f4cd7e512b0a945598ec5a3dc56cfec
KelinSilva <kasvillarrealgmail.com>
2019-04-26 16:56:11 -0600

    admin changes

--- FrondEndAvantiParking/src/app/app-routing.module.ts
+++ FrondEndAvantiParking/src/app/app-routing.module.ts
@@ -7,7 +7,11 @@ import { ReserveComponent } from './components/reserve/reserve.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { HomeComponent } from './components/home/home.component';
 import { AdminHomeComponent } from './components/administration/admin-home/admin-home.component';
- 
+import { AdminParkingsComponent } from './components/administration/admin-parkings/admin-parkings.component';
+import { AdminHeadQuartersComponent } from './components/administration/admin-head-quarters/admin-head-quarters.component';
+import { AdminAdministratorsComponent } from './components/administration/admin-administrators/admin-administrators.component';
+import { AdminUsersComponent } from './components/administration/admin-users/admin-users.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
 const routes: Routes = [
   //All router here 
   { path: '' , component:HomeComponent },
@@ -16,6 +20,11 @@ const routes: Routes = [
   { path: 'user/login' , component: LoginComponent },
   { path: 'hero' , component: HeroComponent },
   { path: 'administration/admin-home' , component: AdminHomeComponent},
+  { path: 'administration/admin-parkings', component: AdminParkingsComponent},
+  { path: 'administration/admin-administrators', component: AdminAdministratorsComponent},
+  { path: 'administration/admin-head-quarters', component: AdminHeadQuartersComponent},
+  { path: 'administration/admin-users', component:AdminUsersComponent},
+  { path: 'notifications', component: NotificationsComponent},
   { path: '**' , component: Errorpage404Component }
 ];
 
