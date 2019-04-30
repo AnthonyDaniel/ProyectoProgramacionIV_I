61c21c1d4838bd674585ea03e4a514b71e5e888d
AnthonyDaniel <39199700+AnthonyDaniel@users.noreply.github.com>
2019-04-30 03:51:07 -0600

    register validatiion

--- FrondEndAvantiParking/src/app/components/user/register/register.component.ts
+++ FrondEndAvantiParking/src/app/components/user/register/register.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  register: FormGroup;
+  constructor(private frmBuilder: FormBuilder) {
+    this.register = this.frmBuilder.group({
+      name: ['Antonio', [Validators.required, Validators.minLength(3)] ],
+      lastName: ['Marin', [Validators.required, Validators.minLength(3)] ],
+      email: ['example@gmail.com', [Validators.required, Validators.pattern("[^ @]*@[^ @]*"), Validators.minLength(3)]],
+      phone: ['90909090', [Validators.required] ],
+      password: ['********', [Validators.required, Validators.minLength(8)]]
+    });
+  }
 
   ngOnInit() {
   }
 
+  
+  onSubmit(p){
+    console.log(p);
+  }
+  
 }
