8465d913a2f3d4fd50cbbbcea3067e3b73f241e0
KelinSilva <kasvillarrealgmail.com>
2019-05-31 00:03:03 -0600

    jhjklm

--- BackEndAvantiParking/app/Http/Controllers/VehicleController.php
+++ BackEndAvantiParking/app/Http/Controllers/VehicleController.php
@@ -6,19 +6,28 @@ use Illuminate\Http\Request;
 use App\Vehicle;
 use App\User;
 use App\Http\Requests\RegisterVehicleRequest;
+use Illuminate\Support\Facades\DB;
 
 class VehicleController extends Controller
 {
 
     public function saveVehicle(RegisterVehicleRequest $request){
-        $user = User::whereEmail($request->users)->first();
+       // $user = User::whereEmail($request->users)->first();
     
-        if (is_null($user)) {
-            return response()->json(['error' => 'Not found'], 401);
-        }else{
-            Vehicle::create($request->all());
-            return response()->json(['data' => 'Create vehicle success'], 200);
-        }
+        //if (is_null($user)) {
+        //    return response()->json(['error' => 'Not found'], 401);
+        //}else{
+          //  Vehicle::create($request->all());
+            //return response()->json(['data' => 'Create vehicle success'], 200);
+        //}
+        DB::insert('INSERT INTO vehiculo(placa, marca, modelo,imagen,users) VALUES(?,?,?,?,?)',[
+            $request->placa,
+            $request->marca,
+            $request->modelo,
+            $request->imagen,
+            $request->users,
+        ]);
+        return $request;
     }
     public function getVehicle(){
 
@@ -30,14 +39,19 @@ class VehicleController extends Controller
 
         //return response()->json(['data' => 'Update vehicle success'], 200);
        
+       
     }
      public function deleteVehicle(RegisterVehicleRequest $request){
+var_dump('sdsd');
 
         //return $request;
-        return $request;
+        
         //$vehicle = Vehicle::where('placa',$lPlate)->delete($request->all());
 
         //return response()->json(['data' => 'Delete vehicle success'], 200);
+      
+        DB::table('vehiculo')->where('placa', '=', 'saa')->delete();
+        return $request;
     }
      
 }
