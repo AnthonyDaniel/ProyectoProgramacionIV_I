d37ec33c4c27362edba509ee778f205668756044
KelinSilva <kasvillarrealgmail.com>
2019-06-09 01:08:24 -0600

    updates reserve
    no verifica bien la hora :c

--- BackEndAvantiParking/app/Http/Controllers/ReserveController.php
+++ BackEndAvantiParking/app/Http/Controllers/ReserveController.php
@@ -13,10 +13,45 @@ class ReserveController extends Controller
     { }
 
     public function addR(ReservarRequest $r){
-        $space= Reserve::whereEspacio($r->espacio)->first();
 
-        if(is_null($space)){
-            try {
+        $space= Reserve::where('espacio', $r->espacio)->first();
+        $date= Reserve::where('fechaReserva', $r->fechaReserva)->first();
+        $eTime= Reserve::where('horaFinal', $r->horaFinal)->get();
+
+      if(is_null($space)){
+        try {
+            DB::insert('INSERT INTO reserva(fechaReserva,horaInicio,horaFinal,espacio,users,vehiculo) VALUES(?,?,?,?,?,?)', [
+                $r->fechaReserva,
+                $r->horaInicio,
+                $r->horaFinal,
+                $r->espacio,
+                $r->users,
+                $r->vehiculo
+            ]);
+            
+           // Reserve::create($r->all());
+            return  response()->json(['data' => 'Added successfully'], 200);
+        } catch (\Illuminate\Database\QueryException $e) {
+            return  response()->json(['error' => $e], 409);
+        }
+      }else{
+          if (is_null($date)) {
+           // return  response()->json(['data' => 'date  no exists'], 409);
+            DB::insert('INSERT INTO reserva(fechaReserva,horaInicio,horaFinal,espacio,users,vehiculo) VALUES(?,?,?,?,?,?)', [
+                $r->fechaReserva,
+                $r->horaInicio,
+                $r->horaFinal,
+                $r->espacio,
+                $r->users,
+                $r->vehiculo
+            ]);
+                return  response()->json(['data' => 'Added successfully'], 200);
+        
+          }else{
+         
+            if ($r->horaInicio > $eTime) {
+                // return  response()->json(['data' => 'time is good '], 409);
+                // Reserve::create($r->all());
                 DB::insert('INSERT INTO reserva(fechaReserva,horaInicio,horaFinal,espacio,users,vehiculo) VALUES(?,?,?,?,?,?)', [
                     $r->fechaReserva,
                     $r->horaInicio,
@@ -25,15 +60,15 @@ class ReserveController extends Controller
                     $r->users,
                     $r->vehiculo
                 ]);
-               // Reserve::create($r->all());
                 return  response()->json(['data' => 'Added successfully'], 200);
-            } catch (\Illuminate\Database\QueryException $e) {
-                return  response()->json(['error' => $e], 409);
+            } else {
+                 return  response()->json(['data' => 'time is higer'], 409);
             }
-        }else{
-            return  response()->json(['error' => 'Exists'], 406);
-        }
-        
+            //return  response()->json(['data' => 'date exists'], 409);
+          }
+        return  response()->json(['data' => 'exists'], 409);
+      }
+          
     }
     public function list(Request $r){
         $data= Reserve::where('users',$r->users)->get();
